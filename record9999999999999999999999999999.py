32533ec51796d0b5a7417100395902fd6b472e64
oybek777 <153380033+oybekvip777@users.noreply.github.com>
2023-12-16 10:55:06 +0500

    Add files via upload

---
+++ record9999999999999999999999999999.py
@@ -0,0 +1,846 @@
+# kalkulatro akt 1
+
+# ext 1 PRINT
+# 1
+# print(4+5)
+# 2
+# print(200-100)
+# 3
+# print(6-7)
+
+# ext 2 z=
+
+# 1
+# z=3
+# e=23
+# print(e*z)
+
+# 2
+# f=400
+# p=500
+# print(p/f)
+
+# 3
+# c=1000
+# o=3
+# print(c**o)
+
+# ext 3 input()
+
+# 1
+# r=int(input('a='))
+# f=int(input('h='))
+# j=r%f
+# print(j)
+
+# 2
+# f=int(input('q='))
+# e=int(input('h='))
+# h=f**e
+# print(h)
+
+# 3
+# f=int(input('h='))
+# w=int(input('y='))
+# s=f-w
+# print(s)
+
+# akt 2 str()
+
+# ism='oybek'
+# famila='komiljonov'
+# nema="ulug'bek o'g'li"
+
+# ext 2 index str()
+
+# ism='oybek'
+# famila='komiljonov'
+# nema="ulug'bek o'g'li"
+
+# 1
+# d=nema[10]
+# 2
+# h=famila[6]
+# 3
+# k=ism[2]
+
+# print(d)
+# print(h)
+# print(k)
+
+# ext 3 str() qoshish
+# 1
+# i='oy'
+# sm='bek'
+# print(i+sm)
+
+# 2
+# i='sobi'
+# sm='njon'
+# print(i+sm)
+
+# 3
+# fam='komil'
+# ila='jonov'
+# print(fam+ila)
+
+# ext 4 len
+
+# 1
+# leni='oybek komiljonov'
+# print(len(leni))
+
+# 2
+# hello="ulug'bek sobirov"
+# print(len(hello))
+
+# 3
+# good_bye='mastura sobirova'
+# print(good_bye)
+
+# ext 5 find
+
+# ism='oybek komiljonov'
+# r=ism.find('kom')
+# print(d)
+
+# ism='komiljonov sobirjon'
+# q=ism.find('jonov')
+# print(d)
+
+# ism='sobirova nuriya'
+# s=ism.find('riya')
+# print(d)
+
+# ext 6 index yordamida harf topish
+
+# ism=''
+# d=ism.index('i',3,5)
+# print(d)
+
+# ism='sobirjon'
+# d=ism.index('j',2,6)
+# print(d)
+
+# ism='komil'
+# d=ism.index('o',0,4)
+# print(d)
+
+# ext 7 isdigit str()
+
+# a='00000111122232334567890'
+# d=a.isdigit()
+# print(d)
+
+# a="79767"
+# d=a.isdigit()
+# print(d)
+
+# a='2010'
+# c=a.isdigit()
+# print(c)
+
+# ext 8 isalpha str()
+
+# ism='oybek'
+# d=ism.isalpha()
+# print(d)
+
+# ism='sobir'
+# d=ism.isalpha()
+# print(d)
+
+# ism='nuriya'
+# d=ism.isalpha()
+# print(d)
+
+# ext 9 isalnum str()
+
+# ism='4325'
+# d=ism.isalnum()
+# print(d)
+
+# ism='dsadswdedf'
+# d=ism.isalnum()
+# print(d)
+
+# ism='dsadswdedf4325'
+# d=ism.isalnum()
+# print(d)
+
+
+# ext 10 ispace str()
+
+# ism='  .  '
+# d=ism.isspace()
+# print(d)
+
+# ism='  .  '
+# d=ism.isspace()
+# print(d)
+
+# ism='  .  '
+# d=ism.isspace()
+# print(d)
+
+# ext 11 istitle str()
+
+# ism='my'
+# d=ism.istitle()
+# print(d)
+
+
+# ism='you'
+# d=ism.istitle()
+# print(d)
+
+
+# ism='print'
+# d=ism.istitle()
+# print(d)
+
+# ext 12 title str()
+
+# ism='my nemas oybek'
+# d=ism.istitle()
+# print(d)
+
+# ism='hello my frendi'
+# d=ism.istitle()
+# print(d)
+
+# ism='hello world'
+# d=ism.istitle()
+# print(d)
+
+# ext 12 capitaliz str()
+
+# ism='nuriya'
+# d=ism.capitalize()
+# print(d)
+
+# ism='sobirjon'
+# d=ism.capitalize()
+# print(d)
+
+# ism='otbek'
+# d=ism.capitalize()
+# print(d)
+
+# akt 3 <>=
+
+# yosh=23
+# yosh=67   
+# d=yosh1<yosh
+# print(d)
+
+
+# yosh=23
+# yosh1=2
+# d=yosh1<yosh
+# print(d)
+
+
+
+# yosh=23
+# yosh1=2010
+# d=yosh1<yosh
+# print(d)
+
+# ext 1 true,False and
+
+# a=True
+# k=False
+
+# d=k and a
+# print(d)
+
+# a=True
+# k=False
+
+# d=k and a
+# print(d)
+
+# a=True
+# k=False
+
+# d=k and a
+# print(d)
+
+# ext 2 or
+
+# k=False
+# a=True
+
+# d=k or a
+# print(d)
+# --------------------------------
+# k=False
+# a=True
+
+# d=k or a
+# print(d)
+# --------------------------------
+# k=False
+# a=True
+
+# d=k or a
+# print(d)
+
+# ext 3 ><= and
+
+# a=20
+# b=13
+# c=0
+
+# d=a>b and b>c
+# print(d)
+
+# a=20
+# b=13
+# c=0
+# -------------------------------
+# d=a>b and b>c
+# print(d)
+
+# a=20
+# b=13
+# c=0
+# ----------------------------
+# d=a>b and b>c
+# print(d)
+
+# ext 4 ism not in 
+
+# son=[oyebk','mastura',"ulug'bek",'sobirjon','nuriya']
+# ism1='sobirjon'
+
+
+
+# son=[oyebk','mastura',"ulug'bek",'sobirjon','nuriya']
+# ism1='mastura'
+
+
+# son=['oyebk','mastura',"ulug'bek",'sobirjon','nuriya']
+# ism1='oybek'
+
+# ext 5 type
+
+# a=23
+# d=type(a)
+# print(d)
+
+# a='oybek'
+# d=type(a)
+# print(d)
+
+# a=665.23
+# d=type(a)
+# print(d)
+
+# akt 4 list
+
+# sonlar=[2010,2000,1991,1915]
+# sonlar[0]=34
+# print(sonlar)
+
+# sonlar=[1,2,3,4,4,5,5,6,]
+# sonlar[5]=100
+# print(sonlar)
+
+# sonlar=[2010,1983,1981,2013,2019]
+# sonlar[4]=1963
+# print(sonlar)
+
+# ext 1 append
+
+# sonlar=[1,2,3,4,5,6,]
+# sonlar.append('oybek')
+# print(sonlar)
+
+# son=[1,2]
+# son.append('alaykum')
+# print(sonlar)
+
+# sonlar=[1,2,3,]
+# sonlar.append('Assalomu')
+# print(sonlar)
+
+
+# ext 2 extend
+
+# sonlar=9,8,7,6,5,4]
+# sonlar1=[1,2,3]
+
+# sonlar1.extend(sonlar)
+# print(sonlar1)
+# ---------------------------------
+# sonlar=[-1,-2,-3,-4,-5,-6,]
+# sonlar1=[-1,-2,-3]
+
+# sonlar1.extend(sonlar)
+# print(sonlar1)
+# ---------------------------------
+# sonlar=[1,2,3,4,5,6,]
+# sonlar1=[1,2,3]
+
+# sonlar1.extend(sonlar)
+# print(sonlar1)
+
+# ext 3 inssert
+
+# sonlar=[1,2,3,4,5,6,]
+
+# sonlar.insert(5,7)
+# print(sonlar)
+
+# sonlar=['oybek','komil']
+
+# sonlar.insert(0,'alik')
+# print(sonlar)
+
+# sonlar=[1,2,3,4,5,6,]
+
+# sonlar.insert(1,10)
+# print(sonlar)
+
+# ext 4 remove
+
+# sonlar=[1,2,3,4,5,6,]
+
+# sonlar.remove(5)
+# print(sonlar)
+# --------------------------------
+# sonlar=[2010,2020,2030,2040,2050]
+
+# sonlar.remove(0)
+# print(sonlar)
+# ---------------------------------
+# sonlar=[1991,1992,1993,1994]
+
+# sonlar.remove(1992)
+# print(sonlar)
+
+# ext 5 pop
+
+# sonlar=[1,2,3,4,5,6,]
+
+# sonlar.pop(4)
+# print(sonlar)
+
+# sonlar=[2010,2013,2019]
+
+# sonlar.pop(1)
+# print(sonlar)
+
+
+# sonlar=['oybek','komil','shahnoza','xafiza','ustoz']
+
+# sonlar.pop(4)
+# print(sonlar)
+
+# ext 5 count
+
+# a=[41,41,41,41,41,41]
+# d=a.count(41)
+# print(d)
+
+# a=[2000,2001,2000,2002,2002,2000]
+# d=a.count(2000)
+# print(d)
+
+
+# d=a.count(2002)
+# print(d)
+
+# akt 5 tuple
+
+# a=(1,2,3,4,5423,3,3,3,3,4,3)
+# b=a.count(3)
+# print(b)
+
+# a=(2000,2000,20034,2000,30004003,2200304,2000)
+# b=a.count(2000)
+# print(b)
+
+# a=(1,2,3,33,3,43,3)
+# b=a.count(3)
+# print(b)
+
+# ext 1 index
+
+# a=(2000,2001,2002,2003,2004,2005,2006,2007)
+# b=a.index(4)
+# print(b)
+
+# a=(1,2,3,4,5,6,7,8,9)
+# b=a.index(7)
+# print(b)
+
+# a=(3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3)
+# b=a.index(4,8,20)
+# print(b)
+
+# akt 6 set
+
+# a={1,2,3,4,5,6}
+# w=a.add(7)
+# print(w)
+# -----------------------
+# a={2001,2002,2003,2004,2005}
+# e=a.add(2006)
+# print(e)
+# -----------------------
+# a={1,2,3,4,5,6}
+# q=a.add(100)
+# print(q)
+
+# ext 1 update
+
+# a={1,2,3,4,5,6}
+# b={'oybek','sobirjon','nuria'}
+# b.update(a)
+# print(b)
+# ---------------------
+# a={1,2,3,4,5,6}
+# b={7,8,9,10}
+# b.update(a)
+# print(b)
+# -----------------------
+# a={1}
+# b={2,3,4,5,6,7,8,9,10}
+# b.update(a)
+# print(b)
+
+# ext 2 remove
+
+# a={11,22,33,44,55,66,77,88}
+# a.remove(77)
+# print(a)
+
+# a={1999,2000,2001,2002,2003,2004,2005}
+# a.remove(1999)
+# print(a)
+
+# a={1,2,3,4,5,6,7,8,9,10}
+# a.remove(1)
+# print(a)
+
+# ext 3 discard
+
+# a={1,2,3,4,5,6,8,7}
+# a.discard(-1)
+# print(a)
+
+# a={1999,1998,1997,1996,1995,1994,1993}
+# a.discard(-1)
+# print(a)
+
+# a={1,2,3,4,5,6,7,8,9,10,11}
+# a.discard(5)
+# print(a)
+
+# akt 7 dicition
+
+# full_data={'ism':'oybek','age':13,'familya':'komiljonov','level class':8}
+# result=full_data.get('age','13 ma\'lumot bor')
+# print(result)
+
+# full_data={'ism':'oybek','age':13,'familya':'komiljonov','level class':8}
+# result=full_data.get('ism','uka ma\'lumot yo\'q')
+# print(result)
+
+# full_data={'ism':'oybek','age':13,'familya':'komiljonov','level class':8}
+# result=full_data.get('ism','oybek ma\'lumot bor')
+# print(result)
+
+# ext 1 key
+
+# full_data={'ism':'sobirjon','age':10,'familya':'komiljonov','level class':5}
+# d=full_data.keys()
+# print(d)
+
+# full_data={'ism':'oybek','age':13,'familya':'komiljonov','level class':8}
+# d=full_data.keys()
+# print(d)
+
+# full_data={'ism':'mastura','age':40,'familya':'sobirova','level class':no}
+# d=full_data.keys()
+# print(d)
+
+# ext 2 valuies
+
+# full_data={'ism':'sobirjon','age':10,'familya':'komiljonov','level class':5}
+# d=full_data.values()
+# print(d)
+
+# full_data={'ism':'oybek','age':13,'familya':'komiljonov','level class':8}
+# d=full_data.values()
+# print(d)
+
+# full_data={'ism':'mastura','age':40,'familya':'sobirova','level class':no}
+# d=full_data.values()
+# print(d)
+
+# akt 8 if elif else
+
+# a=int(input('a='))
+# b=int(input('b='))
+# if a==b:
+#     a=a*b
+#     print(a)
+# elif a==b:
+#     a=a/b
+#     print(a)
+
+# else:
+#     print('notogiri son ')
+
+# a=int(input('a='))
+# b=int(input('b='))
+# if a==b:
+#     a=a*b
+#     print(a)
+# else:
+#     a=a/b
+#     print(a)
+
+# a=int(input('a='))
+# b=int(input('b='))
+# if a==b:
+#     a=a-b
+#     print(a)
+# else:
+#     a=a+b
+#     print(a)
+
+# akt 9 while
+
+# mylist=[]
+
+# s=0
+# while s<'ism':
+#     mylist.append(s)
+#     s=s+1
+#     print(mylist)
+
+# mylist=[]
+
+# e=0
+# while e<'oybek':
+#     mylist.append(e)
+#     e=e+1
+#     print(mylist)
+
+# mylist=[]
+
+# r=0
+# while r<2010:
+#     mylist.append(r)
+#     r=r+1
+#     print(mylist)
+
+
+# akt 10 for 
+
+# h='oybek'
+
+# for i in h:
+#     if i=='o':
+#         print(i)
+#     elif i=='k':
+#         print(i)
+# ---------------------------------------------------------
+# for i in range(1,6):
+#     d=0
+#     while d<=4:
+#         d=d+1
+#         print(i)
+# --------------------------------------------------------
+# oila=['mastura',"ulug'bek",'sobirjon','oybek','nuriya']
+
+# for i in oila:
+#     print(i)
+
+# akt 11 def
+
+# def my_mother_father():
+#     print("mastura","ulug'bek")
+
+# print(my_mother_father())
+# -------------------------------------------------------------
+# my_family=['mastura',"ulug'bek",'oybek','sobirjon','nuriya']
+
+# def family():
+#     for i in my_family:
+#         print(i)
+# ----------------------------------------------------------------
+# toq_son=[]
+# juft_son=[]
+
+# def my_list():
+#     for i in list(range(1,21)):
+#         if i %2==0:
+#             juft_son.append(i)
+#         else:
+#             toq_son.append(i)
+        
+#         print("toq_sonlar:",toq_son)
+#         print("juft_sonlar",juft_son)
+
+# print(my_list())
+
+# akt 12 time
+
+# from  datetime import datetime,timedelta
+
+# h=datetime.now()
+# print(hozirgi_vaqt)
+
+# hozirgi_=datetime.now()
+# print(hozirgi_vaqt.year)
+
+# hozirgi_vaqt=datetime.now()
+# print(hozirgi_vaqt.month)
+
+# ext 1 replase
+# rep=datetime.now().replace()
+# print(replace)
+
+# repla=datetime.now().replace(year=1915)
+# print(replace)
+
+# replace=datetime.now().replace(month=5)
+# print(replace)
+
+# ext 2 strftime
+
+# strftime=datetime.now().strftime("%w")
+# print(strftime)
+
+# strf=datetime.now().strftime("%d")
+# print(strftime)
+
+# str=datetime.now().strftime("%Y")
+# print(strftime)
+
+# ext 3 timedelta
+
+# vaqt=timedelta(hours=19,minutes=30,seconds=0)
+# vaqtanig_sng=timedelta(hours=21)
+# v=vaqtanig_song-vaqt
+# print(v)
+
+# vaqt=timedelta(hours=23,minutes=23)
+# vaqtanig_sog=timedelta(hours=1)
+# v=vaqtanig_song+vaqt
+# print(v)
+
+# vaqt=timedelta(hours=4,minutes=30)
+# vaqtanig_son=timedelta(hours=6,minutes=30)
+# v=vaqtanig_song+vaqt
+# print(v)
+
+# ext 4 calendar
+
+# kalendar_Xw=calendar.calendar(theyear=2023,m=1,w=5)
+# print(kalendar_XX)
+
+# kalendar_Xe=calendar.calendar(theyear=1915,m=3,w=0,l=2)
+# print(kalendar_XX)
+
+# kalendar_Xq=calendar.calendar(theyear=2030,l=1,m=1)
+# print(kalendar_XX)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
